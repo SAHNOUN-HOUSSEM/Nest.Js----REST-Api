5027ebeb946b67a9a444d7d65cf7a70ad8c38f47
SahnounHoussem <sahnoun.houssem05@gmail.com>
2024-03-28 22:25:20 +0100

    created users service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-
-let users = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: "john@gmail.com",
-        role: "ADMIN"
-    },
-    {
-        id: 2,
-        name: 'Jane Doe',
-        email: "jane@gmail.com",
-        role: "ENGINEER"
-    },
-    {
-        id: 3,
-        name: 'Jim Doe',
-        email: "jim@gmail.com",
-        role: "MANAGER"
-    },
-    {
-        id: 4,
-        name: 'Jack Doe',
-        email: "jack@gmail.com",
-        role: "ADMIN"
-    },
-]
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users') // /users
 export class UsersController {
+
+    constructor(private readonly userService: UsersService) { }
+
+
     // GET /users -- get all users
     // GET /users/:id -- get user with specific id
     // POST /users -- create user
@@ -36,40 +14,31 @@ export class UsersController {
     // DELETE /users/:id -- delete user with specific id
 
     @Get() // GET /users
-    findAll() {
-        return users;
+    findAll(@Query("role") role?: "ADMIN" | "ENGINEER" | "MANAGER") {
+        return this.userService.findAll(role)
     }
 
     @Get("/:id") // GET /users/:id
-    findOne(@Param("id") id: number) {
-        return users.find(user => user.id == id)
+    findOne(@Param("id") id: string) {
+        return this.userService.findOne(+id)
     }
 
     @Post() //POST /users
     create(@Body() user: { name: string, email: string, role: string }) {
-        const id = users.length + 1
-        const newUser = { id, ...user }
-        users.push(newUser)
-        return newUser
+        return this.userService.create(user)
     }
 
     @Put("/:id") // PUT /users/:id
     update(
-        @Param("id") id: number,
-        @Body() user: { name: string, email: string, role: string }
+        @Param("id") id: string,
+        @Body() user: { name?: string, email?: string, role?: string }
     ) {
-        const updatedUser = { id, ...user }
-        users = users.map(u => {
-            return u.id == id ? updatedUser : u
-        }
-        )
-        return updatedUser
+        return this.userService.update(+id, user)
     }
 
     @Delete("/:id") // DELETE /users/:id
-    delete(@Param("id") id: number) {
-        users = users.filter(user => user.id != id)
-        return users
+    delete(@Param("id") id: string) {
+        return this.userService.delete(+id)
     }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,65 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users = [
+        {
+            id: 1,
+            name: 'John Doe',
+            email: "john@gmail.com",
+            role: "ADMIN"
+        },
+        {
+            id: 2,
+            name: 'Jane Doe',
+            email: "jane@gmail.com",
+            role: "ENGINEER"
+        },
+        {
+            id: 3,
+            name: 'Jim Doe',
+            email: "jim@gmail.com",
+            role: "MANAGER"
+        },
+        {
+            id: 4,
+            name: 'Jack Doe',
+            email: "jack@gmail.com",
+            role: "ADMIN"
+        },
+    ]
+
+
+    findAll(role?: "ADMIN" | "ENGINEER" | "MANAGER") {
+        if (role)
+            return this.users.filter(user => user.role == role)
+        return this.users
+    }
+
+    findOne(id: number) {
+        return this.users.find(user => user.id == id)
+    }
+
+    create(user: { name: string, email: string, role: string }) {
+        const id = this.users.length + 1
+        const newUser = { id, ...user }
+        this.users.push(newUser)
+        return newUser
+    }
+
+    update(id: number, user: { name?: string, email?: string, role?: string }) {
+        this.users = this.users.map(u =>
+            u.id == id ? { ...u, ...user, } : u
+        )
+        const updatedUser = this.users.find(user => user.id == id)
+        return updatedUser
+    }
+
+    delete(id: number) {
+        const deletedUser = this.users.find(u => u.id == id)
+        this.users = this.users.filter(user => user.id != id)
+        return deletedUser
+    }
+
+}
